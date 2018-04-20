a97914b3c0855047f2ed22dfe978e9007612091d
Sandy <sandy.sng@hotmail.com>
2018-04-20 18:42:52 +0800

    Add cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Caching the Inverse of a Matrix
+## Matrix inversion is usually a costly computation and there may be some benefit 
+## to caching the inverse of a matrix rather than compute it repeatedly.
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+# makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
 
+makeCacheMatrix <- function(x = matrix()){
+        invrs <- NULL 
+        set <- function(y) { 
+                x <<- y 
+                invrs <<- NULL 
+                }
+        get <- function() x 
+        setinverse <- function(inverse) invrs <<- inverse 
+        getinverse <- function() invrs 
+        list(set = set, 
+        get = get, 
+        setinverse = setinverse,
+        getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+# cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then the cachesolve should retrieve the inverse from the cache.
+## Return a matrix that is the inverse of 'x'
+## This function assumes that the matrix is always invertible.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        invrs <- x$getinverse()
+        # If the inverse has already been calculated
+        if (!is.null(invrs)) {
+                # get it from the cache and skips the computation.
+                message("getting cached data")
+                return(invrs)
+        }
+        # Otherwise, calculates the inverse 
+        data <- x$get()
+        invrs <- solve(data, ...)
+        
+        # Sets the value of the inverse in the cache via the setinv function.
+        x$setinverse(invrs)
+        invrs
 }
+
